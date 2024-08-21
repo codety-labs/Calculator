438c8232fb33803e3da0ed23609042273e43b148
Anonymous <anonymous@anonymous>
2024-08-21 15:54:04 -0700

    Added temporary code changes for demonstrating the Codety code review comments.

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -5,7 +5,7 @@ import com.houarizegai.calculator.ui.CalculatorUI;
 import java.util.List; //added as example for presenting the Codety code review comments.
 public class App {
 
-    int hello = 0; //added as example for presenting the Codety code review comments.
+    private int hello = 0; //added as example for presenting the Codety code review comments.
 
     public static void main(String[] args) {
         new CalculatorUI();
